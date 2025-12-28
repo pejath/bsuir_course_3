fb8ffd1b6a90e573f74a2df1682890b28e5f94d6
pejath <pejath@yandex.ru>
2025-12-28 13:23:20 +0300

    ngrok setup

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'http://127.0.0.1:5173'
+    origins 'http://localhost:5173', 'http://127.0.0.1:5173', 'https://pejath.cloud-ip.cc', 'http://pejath.cloud-ip.cc'
     
     resource '*',
       headers: :any,
